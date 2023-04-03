b52407baeba72918da148af3bbdeb68e21b39d1a
Bergen Kendziorski <kendzi23@msu.edu>
2023-04-02 23:20:26 -0400

    outdated files
    adding a couple files that are outdated, but this needed to be done
    before a rebase

--- macro/e12014/bergen/enloss.C
+++ macro/e12014/bergen/enloss.C
@@ -7,8 +7,17 @@
 #include "/mnt/analysis/e12014/bergen/ATTPCROOTv2/AtGenerators/AtTPCFissionGeneratorV3.h"
 #endif
 
+
+//function declarations:
+void enlos();
+void open_table();
+
+//variable declarations:
+
 void enloss()
 {
+   open_table();
+
    int num_points = 10;
    int eloss = 0;
    double energy1 = 500;
@@ -21,7 +30,37 @@ void enloss()
       energy1 -= eloss;
    }
 
-   auto gen = new AtTPCFissionGeneratorV3();
+   //auto gen = new AtTPCFissionGeneratorV3();
+}
+
+void open_table()
+{
+   vector<vector<double>> content;
+   vector<double> row;
+   string line, word;
+   double value;
+
+   fstream file ("PbinHe.csv", ios::in);
+   if(file.is_open())
+   {
+      while(getline(file,line))
+      {
+         row.clear();
+	 stringstream str(line);
+
+	 while(getline(str, word, ','))
+	 {
+	    row.push_back(stod(word));
+	 }
+
+	 content.push_back(row);
+      }
+   }
+   else
+   {
+      cout << "file not found" << endl;
+   }
+
 }
 
 #endif
